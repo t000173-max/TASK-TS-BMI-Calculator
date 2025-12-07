22ccc19f0cbe6514451314d66a9d4e30fe47b0b0
t000173-max <t000173@coded.edu.kw>
2025-12-08 01:52:23 +0300

    done1

--- src/BMI.ts
+++ src/BMI.ts
@@ -1,52 +1,62 @@
 /**
  * Calculate BMI
- * - Accepts weight in KG as a parameter (type number)
- * - Accepts height in CM as a parameter (type number)
- * - returns the calculated BMI (type number)
- * - The equation for BMI is (weight (kg) / height^2(m)).
  */
 function calculateBMI(weight: number, height: number): number {
-  // Write your code here 👇🏼
-  const BMI = 0;
-  // Stop making changes here!
-  return BMI;
+    const heightMeter = height / 100;
+    const BMI = weight / (heightMeter * heightMeter);
+    return BMI;
 }
 
 /**
  * Get BMI State
- * - Accepts BMI as a parameter (type number)
- * - returns a BMIStatus type indicating the BMI category (type BMIStatus)
  */
-
-// Don't change this type
 type BMIStatus = "underweight" | "healthy" | "overweight" | "obese";
 
 function getBMIState(BMI: number): BMIStatus {
-  // Don't change this variable
-  let BMIState: BMIStatus = "obese";
-  // Write your code here 👇🏼
+    let BMIState: BMIStatus = "obese";
 
-  // Stop making changes here!
-  return BMIState;
+    if (BMI < 18.5) {
+        BMIState = "underweight";
+    } else if (BMI < 25) {
+        BMIState = "healthy";
+    } else if (BMI < 30) {
+        BMIState = "overweight";
+    } else {
+        BMIState = "obese";
+    }
+
+    return BMIState;
 }
 
 /**
- * Get BMI State by Age
- * - Accepts BMI as a parameter (type number)
- * - Accepts age as a parameter (type number)
- * - returns a HealthStatus type indicating the BMI state (type HealthStatus)
+ * Get BMI State By Age
  */
-
-// Don't change this type
 type HealthStatus = "not healthy" | "healthy";
 
 function getBMIStateByAge(BMI: number, age: number): HealthStatus {
-  // Don't change this variable
-  let BMIState: HealthStatus = "not healthy";
-  // Write your code here 👇🏼
+    const ageRanges = [
+        { minAge: 19, maxAge: 24, minBMI: 19, maxBMI: 24 },
+        { minAge: 25, maxAge: 34, minBMI: 20, maxBMI: 25 },
+        { minAge: 35, maxAge: 44, minBMI: 21, maxBMI: 26 },
+        { minAge: 45, maxAge: 54, minBMI: 22, maxBMI: 27 },
+        { minAge: 55, maxAge: 64, minBMI: 23, maxBMI: 28 },
+        { minAge: 65, maxAge: Infinity, minBMI: 24, maxBMI: 29 },
+    ];
 
-  // Stop making changes here!
-  return BMIState;
+    const healthyRange = ageRanges.find(
+        ({ minAge, maxAge }) => age >= minAge && age <= maxAge
+    );
+
+    if (!healthyRange) {
+        return "not healthy";
+    }
+
+    if (BMI <= healthyRange.minBMI || BMI >= healthyRange.maxBMI) {
+        return "not healthy";
+    } else {
+        return "healthy";
+    }
 }
 
 export { calculateBMI, getBMIState, getBMIStateByAge };
+
